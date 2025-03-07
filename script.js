3a54a3834598a568874049a8b8eb9c910f6f04d4
mahdi <mohsseni@gmail.com>
2025-03-07 11:24:10 +0100

    map tocken

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9oc3NlbmkiLCJhIjoiY2t5eGk0aHkyMGlycTJucWlmMHJxNDg1diJ9.BwSHl6Z5IEhNInEw-1uRxw';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
